72d63b42a34b4b8a19c9ea9c4302a5a66419c696
Carl hope <84421131+carlhope@users.noreply.github.com>
2025-01-23 12:36:54 +0000

    Add project files.

---
+++ RecordShopFront.Client/Program.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace RecordShopFront.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
